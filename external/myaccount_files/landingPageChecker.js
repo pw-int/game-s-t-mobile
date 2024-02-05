83d166cf1a31a36009963de49be2b4e5dc27c6db
Ivan Dubynets <ivan.dubynets@gmail.com>
2024-02-05 12:09:00 +0100

    Updates

--- external/myaccount_files/landingPageChecker.js
+++
@@ -1,1 +0,0 @@
-document.addEventListener("DOMContentLoaded",function(){navigator.cookieEnabled||(window.location.href=TwigData.url("error_cookie_disabled"))});
--- external/myaccount_files/phoneMask.js
+++
@@ -1,1 +0,0 @@
-function maskMobileNumberInput(a,b,c){a.unmask().mask(a.attr("data-input-mask"),{translation:{d:{pattern:/\d/},0:"",9:"","#":{pattern:/\d/,recursive:!0},A:{pattern:/[a-zA-Z0-9]/},S:{pattern:/[a-zA-Z]/}}})};
